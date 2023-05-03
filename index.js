8a530a55f7f241d6f66aa782082005d4083fe62b
Tyrell <tysteg14@gmail.com>
2023-05-02 21:03:19 -0500

    creates

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+})
+
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+
+    if (right < 360) {
+        dodger.style.left = `${right + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
